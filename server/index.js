a9856d062bcd8242413f077f6de21a034ec025e1
nay4 <bayani.cruz@gmail.com>
2025-07-21 00:08:53 +1000

    Fix deployment issues

--- server/index.js
+++ server/index.js
@@ -14,7 +14,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors()); // Enable CORS for frontend
+app.use(cors({
+  origin: ['https://bayanicruz.github.io', 'http://localhost:3000'],
+  credentials: true
+})); // Enable CORS for frontend
 app.use(express.json());
 
 // MongoDB connection
